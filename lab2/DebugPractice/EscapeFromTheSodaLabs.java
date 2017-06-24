f762ed7655ab78f0fb7644333ccc0e3e1803b4c5
WhoYoung99 <linking2046.cv97@g2.nctu.edu.tw>
2017-06-24 14:46:43 +0800

    finish puzzle: using F7 F8 and Shift+F8

--- lab2/DebugPractice/EscapeFromTheSodaLabs.java
+++ lab2/DebugPractice/EscapeFromTheSodaLabs.java
@@ -5,6 +5,16 @@ public class EscapeFromTheSodaLabs extends Trial {
      * has "deployed the algorithm" which is set to destroy the labs. To save Soda, collect all the treasures in as few
      * hops as possible. You may set only one breakpoint.
      */
+
+
+    /** Bonus for Bosses #1: Do you see why the program registers 8 hops with the bare minimum constraints?
+     *  Do this while only registering 5 hops with all of the same constraints!
+     *  Ans: put the breakpoint at line 20: the first of treasureChest.
+     *       the key question is: how this program determine isStop()? It's based on last timestamp and current time.
+     *       and, every treasureChest should get a Goal and try to avoid entering for-loop in both traps
+     *       as a result, use shift + F8 to get Goals from trap without entering for-loop,
+     *       and use F8 for plain treasureChest
+    */
     public static void main(String[] args) {
         Traveler cal = new Traveler("General Ackbar", 5, 8);
         treasureChest(cal);
